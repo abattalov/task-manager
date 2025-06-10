81d4dfb36459c4c6545ff7d12b91f6fa3ef57c88
abattalov <abattalov14@gmail.com>
2025-06-09 22:50:39 -0500

    organized folder structure, created a Task type, and rendered tasks onto page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import type { Task } from './types/Task'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const mockTasks: Task[] = [
+    {
+      id: 1,
+      title: "Learn Vite Setup",
+      description: "Set up the project structure",
+      completed: true,
+      createdAt: new Date()
+    },
+    {
+      id: 2,
+      title: "Build task components",
+      completed: false,
+      createdAt: new Date()
+    }
+  ]
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Task Manager</h1>
+      <p>Total tasks: {mockTasks.length}</p>
+      <ul>
+        {mockTasks.map(task => (
+          <li key={task.id}>
+            {task.title} - {task.completed ? '✅' : '❌'}
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
